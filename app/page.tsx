b4514d742022d2ad28e7443d08bdd37ce4090172
Patson T Lawrence <patsont90@gmail.com>
2025-08-15 14:58:25 +0300

    udate

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,19 @@ export default function LoginPage() {
   const handleLogin = async (e: React.FormEvent) => {
   e.preventDefault();
 
-  const res = await fetch('https://stockwell-backend.onrender.com/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      username: 'test',
-      password: 'test',
-    }),
-  });
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+
+const res = await fetch(`${API_URL}/api/login`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    username: 'test',
+    password: 'test',
+  }),
+});
+
 
   if (res.ok) {
     router.push('/request');
