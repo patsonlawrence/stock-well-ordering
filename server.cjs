32c8513907a276c64660cb1ec5f9d59c751860c5
Patson T Lawrence <patsont90@gmail.com>
2025-08-16 20:21:43 +0300

    update

--- server.cjs
+++ server.cjs
@@ -5,14 +5,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.options('*', cors({
-  origin: [
-    'http://localhost:3000',
-    'http://192.168.100.20:3000',
-    'https://stock-well-ordering.vercel.app'
-  ],
-  credentials: true
-}));
 app.use(cors({
   origin: [
     'http://localhost:3000',
@@ -21,13 +13,6 @@ app.use(cors({
   ],
   credentials: true
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://stock-well-ordering.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  next();
-});
 app.use(express.json());
 const pool = new Pool({
   user: process.env.DB_USER,
